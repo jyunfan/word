c3199c559fca2c23ab1d3088b5ce8cc486ae28c6
Jyun-Fan Tsai <jyunfan@gmail.com>
2023-05-21 11:53:14 +0800

    Add new words

--- getimg.py
+++ getimg.py
@@ -3,13 +3,13 @@ import os
 from icrawler.builtin import GoogleImageCrawler
 
 words = [
-    "wolf","web","water","watch",
-    "fox","box","six","wax",
-    "yoyo", "yak", "yogurt", "yacht",
-    "zipper", "zero", "zoo", "zebra"
+"seal","sun","soap","socks",
+"turtle","tent","tiger","teacher",
+"umbrella","up","umpire","uncle",
+"violin","vet","vest","van",
 ]
 
 for word in words:
     os.makedirs('img/'+word, exist_ok=True)
     google_crawler = GoogleImageCrawler(storage={'root_dir': 'img/'+word})
-    google_crawler.crawl(keyword=word, max_num=2)
+    google_crawler.crawl(keyword=word, max_num=3)
